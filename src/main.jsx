bc571ded1b22d3ea196b2b70cdb9040bad73f9bf
faysalhosen <faysanhosen7025@gmail.com>
2023-10-22 11:28:36 +0600

    added basic react project setup

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Root.jsx';
+import Home from './Home.jsx';
+import Demo from './demo.jsx';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/demo',
+        element: <Demo></Demo>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
